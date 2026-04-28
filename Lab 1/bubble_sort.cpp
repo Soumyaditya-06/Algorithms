3a9e167ef5ffd1d5589ed978f615c1610c23b5a1
Soumyaditya Maji <soumyadityamaji06@gmail.com>
2026-04-28 07:46:25 +0530

    Add files via upload

---
+++ Lab 1/bubble_sort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    auto start = high_resolution_clock::now();
+
+    bubbleSort(arr, n);
+
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "\nSorted array:\n";
+    printArray(arr, n);
+
+    cout << "Time taken: " << duration.count() << " microseconds" << endl;
+
+    return 0;
+}
